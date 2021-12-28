2c65db3fc7f8915510e88ecf18e51cdc46bd8bdf
fcVcnte <vcnte.felipe@outlook.com>
2021-12-28 13:24:10 -0300

    Responsive design and description tratament

--- src/App.js
+++ src/App.js
@@ -66,6 +66,12 @@ export default () => {
         Netflix Brand images right <br/>
         Themoviedb data right
       </footer>
+
+      {movieList.length <= 0 &&
+        <div className="loading">
+          <img src="https://media.filmelier.com/noticias/br/2020/03/Netflix_LoadTime.gif"></img>
+        </div>
+      }
     </div>
   );
 }
